b6526a1f9f4b262daf8e1ac475d4ea7d0f959ced
Lucas Kramer <krame505@umn.edu>
2021-05-25 16:54:51 -0500

    Implement move selection

--- static/app.js
+++ static/app.js
@@ -8,6 +8,25 @@ function pieceChar(kind) {
   else if (kind == 'Pawn') return '♟'
 }
 
+var state = null
+var moves = []
+
+function moveFrom(move) {
+  if (move.tag == 'Move' || move.tag == 'Promote') {
+    return move.contents.from
+  } else if (move.tag == 'Castle') {
+    return {rank: state.turn == 'Black'? 0 : 7, file: 4}
+  }
+}
+
+function moveTo(move) {
+  if (move.tag == 'Move' || move.tag == 'Promote') {
+    return move.contents.to
+  } else if (move.tag == 'Castle') {
+    return {rank: state.turn == 'Black'? 0 : 7, file: move.contents.kingSide? 6 : 2}
+  }
+}
+
 function update() {
   $.ajax({url: `status.json`, cache: false, timeout: 3000}).done(
     function (json) {
@@ -30,14 +49,21 @@ function update() {
       }
       for (i = 0; i < 8; i++) {
         board.rows[0].cells[i + 1].innerHTML = (i + 10).toString(36)
+        board.rows[0].cells[i + 1].className = "edgeCell"
         board.rows[9].cells[i + 1].innerHTML = (i + 10).toString(36)
+        board.rows[9].cells[i + 1].className = "edgeCell"
         board.rows[i + 1].cells[0].innerHTML = 8 - i
+        board.rows[i + 1].cells[0].className = "edgeCell"
         board.rows[i + 1].cells[9].innerHTML = 8 - i
+        board.rows[i + 1].cells[9].className = "edgeCell"
       }
       for (rank = 0; rank < 8; rank++) {
         for (file = 0; file < 8; file++) {
-          cell = board.rows[rank + 1].cells[file + 1]
+          const pos = {rank: rank, file: file}
+          var cell = board.rows[rank + 1].cells[file + 1]
           cell.style.backgroundColor = (rank + file) % 2? '#654321' : 'tan'
+          cell.className = "boardCell"
+          cell.onclick = function () { handleSelect(pos) }
           square = state.board[rank][file]
           if (square.occupied) {
             cell.innerHTML = pieceChar(square.piece.kind)
@@ -48,8 +74,55 @@ function update() {
     })
 }
 
-var source = new EventSource('/events')
-source.onmessage = function (event) {
+function doMove(i) {
+  console.log("Move", i)
+}
+
+var selected = null
+var candidates = []
+function handleSelect(pos) {
+  for (rank = 0; rank < 8; rank++) {
+    for (file = 0; file < 8; file++) {
+      cell = board.rows[rank + 1].cells[file + 1]
+      cell.style.backgroundColor = (rank + file) % 2? '#654321' : 'tan'
+    }
+  }
+  square = state.board[pos.rank][pos.file]
+  if (square.occupied && square.piece.color == state.turn && pos != selected) {
+    selected = pos
+    board.rows[pos.rank + 1].cells[pos.file + 1].style.backgroundColor = 'cyan'
+    candidates = []
+    for (i = 0; i < moves.length; i++) {
+      from = moveFrom(moves[i])
+      if (from.rank == pos.rank && from.file == pos.file) {
+        to = moveTo(moves[i])
+        board.rows[to.rank + 1].cells[to.file + 1].style.backgroundColor = 'cyan'
+        candidates.push(i)
+      }
+    }
+  } else if (selected != null) {
+    var narrowed = []
+    candidates.forEach(function (i) {
+      to = moveTo(moves[i])
+      if (to.rank == pos.rank && to.file == pos.file) {
+        narrowed.push(i)
+      }
+    })
+    if (narrowed.length > 0 && narrowed[0].tag == 'Promote') {
+      var promoKind = prompt("Enter kind of promoted piece", "Queen")
+      narrowed = narrowed.filter(move => promoKind != null && move.contents.kind.toLowerCase() == promoKind.toLowerCase())
+    }
+    if (narrowed.length > 0) {
+      doMove(narrowed[0])
+      selected = null
+    }
+  }
+}
+
+var events = new EventSource('/events')
+events.onmessage = function (e) {
+  console.log("Got event", e)
+  event.innerHTML = e
   update()
 }
 
