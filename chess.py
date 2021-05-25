a9739687e8c5abfc813acd35539672920cf71aa1
Lucas Kramer <krame505@umn.edu>
2021-05-25 11:47:54 -0500

    Tweaks to json serialization of C data

--- chess.py
+++ chess.py
@@ -6,17 +6,28 @@ import json
 import msgclient
 from _chess import ffi, lib
 
-def cdata_dict(cd):
+def cdata_dict(cd, ty=None):
     if isinstance(cd, ffi.CData):
-        try:
-            return ffi.string(cd)
-        except TypeError:
-            try:
-                return [cdata_dict(x) for x in cd]
-            except TypeError:
-                return {k: cdata_dict(getattr(cd, k)) for k in dir(cd)}
-    else:
+        ty = ffi.typeof(cd)
+    if ty.kind == 'array':
+        return [cdata_dict(x, ty.item) for x in cd]
+    elif ty.kind == 'struct':
+        fields = dict(ty.fields)
+        if 'tag' in fields:
+            tagName = ffi.string(ffi.cast(fields['tag'].type, cd.tag))[len(ty.cname.split("struct ")[1]) + 1:]
+            if 'contents' in fields:
+                unionFields = dict(fields['contents'].type.fields)
+                if tagName in unionFields:
+                    return {'tag': tagName, 'contents': cdata_dict(getattr(cd.contents, tagName), unionFields[tagName].type)}
+            return tagName
+        else:
+            return {k: cdata_dict(getattr(cd, k), f.type) for k, f in ty.fields}
+    elif ty.kind == 'enum':
+        return ffi.string(ffi.cast(ty, cd))
+    elif ty.kind == 'primitive':
         return cd
+    else:
+        raise ValueError("C type {} cannot be converted to dict representation".format(ty.cname))
 
 class ChessClient(msgclient.Client):
     def __init__(self, serial, callback):
