9d9d8502d65b48a3e85ac7ca0d4538494db1e293
Lucas Kramer <krame505@umn.edu>
2021-06-11 18:42:58 -0500

    Update comment

--- forward-serial-device.sh
+++ forward-serial-device.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Helper script to forward a local serial device to a pseudoterminal on a remote machine via ssh
+# Helper script to forward a local serial device to a pseudoterminal on a remote host via ssh.
+# Example usage:
+#   ./forward-serial-device /dev/ttyUSB* user@hostname.net
+#
+# A new pseudoterminal device will be created under /dev/pty/ on the remote host.
 
 PORT=$1
 HOST=$2
